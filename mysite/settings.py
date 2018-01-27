b8391b1be7ea66865dd62cfb3fe0a8cd532db185
akerkeofficial <akerkeofficial@gmail.com>
2018-01-27 23:39:31 +0600

    third step

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5e*vt9rz$1bx5tvne2xs9uro@1$+ztvqm6l7@8p9xx&lxb4&x4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'happywsbb.pythonanywhere.com']
 
